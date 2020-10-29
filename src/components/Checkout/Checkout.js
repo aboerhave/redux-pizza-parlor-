b2e317583ccfa1ad861f29ef67802278e454863c
lawlernp <lawlernp@gmail.com>
2020-10-29 16:56:27 -0500

    added some pseudocode to Checkout component

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -6,11 +6,32 @@ class Checkout extends Component {
   render(){
     return (
         <>
-        <h1>Step 3: Checkout</h1>
         {/* <h3>import user info here</h3> */}
-        {/* <h2>import order type here</h2> */}
-        {/* table with order info here */}
 
+        <h1>Step 3: Checkout</h1>
+        <h3>
+            this.props.reduxState.reducer.map
+            .customer_name
+            .street_address
+            .cty
+        </h3>    
+        <h2>.type</h2>  
+      {/* <h2>import order type here</h2> */}
+
+        <table>
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Cost</th>
+                </tr>
+            </thead>
+            <tbody>
+                this.props.reduxState.reducer.map => (item)
+                <td>{item.name}</td>
+                <td>{item.cost}</td>
+            </tbody>
+        </table>
+        loop through costs and add them to eachother
         {/* Total: insert total here */}
         {/* Checkout Button */}
         </>
