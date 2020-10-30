549ad2c532e02e11bb150453dbb7c088f74944fe
lawlernp <lawlernp@gmail.com>
2020-10-30 10:57:31 -0500

    added table to admin page and axios request to get orders

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,18 +1,50 @@
-
-import React, { Component } from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Admin extends Component {
-    render() {
-        return (
-            <>
-            </>
-        )}
+
+
+    componentDidMount = () =>{
+        this.getOrders();
+    }
+
+    getOrders = () => {
+        axios.get('/api/order').then((response) => {
+        this.props.dispatch({ type: "GET_ORDERS", payload: response.data });      
+        }).catch((error) => {
+        console.log(error); 
+        })
+    }
+
+  render() {
+    return (
+      <>
+        <table>
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Time Order Placed</th>
+              <th>Type</th>
+              <th>Cost</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.props.reduxStore.allOrders.map((order) => (
+              <tr key={order.id}>
+                <td>{order.customer_name}</td>
+                <td>{order.time}</td>
+                <td>{order.type}</td>
+                <td>{order.total}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </>
+    );
+  }
 }
 
-
-
-const putReduxStateOnProps = (reduxState) => ({
-    reduxState 
-  });
-  export default connect(putReduxStateOnProps)(ArtistList);
+const putReduxStateOnProps = (reduxStore) => ({
+  reduxStore,
+});
+export default connect(putReduxStateOnProps)(ArtistList);
--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,51 +1,77 @@
-import React, {Component} from 'react';
-import {connect} from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class Checkout extends Component {
+  state = {
+    newOrder: {
+      customer_name: "",
+      street_address: "",
+      city: "",
+      zip: "",
+      type: "",
+      total: 0,
+      pizzas: "",
+    },
+  };
+  handleClick = (event) => {
+    console.log("clicked");
+    event.preventDefault();
 
+    this.setState({
+      newOrder: {
+        customer_name: this.props.reduxStore.orderInfo.customer_name,
+        street_address: this.props.reduxStore.orderInfo.street_address,
+        city: this.props.reduxStore.orderInfo.cty,
+        zip: this.props.reduxStore.orderInfo.zip,
+        type: this.props.reduxStore.orderInfo.type,
+        total: 0,
+        pizzas: "",
+      },
+    });
 
-    handleClick = () =>{
-        console.log('clicked');
-        
-    }
+    axios
+      .post("/api/order", this.state.newOrder)
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
 
-  render(){
+  render() {
     return (
-        <>
-        {/* <h3>import user info here</h3> */}
-
+      <>
         <h1>Step 3: Checkout</h1>
         <h3>
-            {this.props.reduxStore.orderInfo.customer_name}<br/>
-            {this.props.reduxStore.orderInfo.street_address}<br/>
-            {this.props.reduxStore.orderInfo.cty}
-        </h3>    
-        <h2>{this.props.reduxStore.orderInfo.type}</h2>  
-
+          {this.props.reduxStore.orderInfo.customer_name}
+          <br />
+          {this.props.reduxStore.orderInfo.street_address}
+          <br />
+          {this.props.reduxStore.orderInfo.cty}
+        </h3>
+        <h2>{this.props.reduxStore.orderInfo.type}</h2>
         <table>
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Cost</th>
-                </tr>
-            </thead>
-            <tbody>
-                {/* this.props.reduxState.reducer.map => (item)
+          <thead>
+            <tr>
+              <th>Name</th>
+              <th>Cost</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* this.props.reduxState.reducer.map => (item)
                 <td>{item.name}</td>
                 <td>{item.cost}</td> */}
-            </tbody>
+          </tbody>
         </table>
         loop through costs and add them to eachother
         {/* Total: insert total here */}
         <button onClick={this.handleClick}>Button One</button>
-
-        </>
-
-        
+      </>
     );
   }
 }
 
-const putReduxStateOnProps = (reduxStore) => ({reduxStore});
+const putReduxStateOnProps = (reduxStore) => ({ reduxStore });
 
 export default connect(putReduxStateOnProps)(Checkout);
